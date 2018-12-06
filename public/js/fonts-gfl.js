e2b4380d1cfd6e8aabe8b7211992deddc5944ee3
o10n-x <info@optimization.team>
2018-12-06 17:25:22 +0100

    core update

--- public/js/fonts-gfl.js
+++ public/js/fonts-gfl.js
@@ -1,1 +1,1 @@
-function mc(a){return a?a:!1}var uc=y[5]?y[5][2]:0,vc=uc?uc[4]:!1,wc=uc?uc[5]:!1;!wc||wc instanceof Array||(wc=[wc]);A.fonts=function(a){function c(){window.WebFont.load(a)}25!==vc?c():K(wc,c)};
+function nc(a){return a?a:!1}var vc=y[5]?y[5][2]:0,wc=vc?vc[4]:!1,xc=vc?vc[5]:!1;!xc||xc instanceof Array||(xc=[xc]);A.fonts=function(a){function c(){window.WebFont.load(a)}25!==wc?c():K(xc,c)};
