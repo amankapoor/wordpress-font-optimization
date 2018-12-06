45183ea3d3f133cf34d1d06d9d9f21d382641030
o10n-x <info@optimization.team>
2018-12-06 11:19:04 +0100

    core update

--- public/js/fonts-gfl.js
+++ public/js/fonts-gfl.js
@@ -1,1 +1,1 @@
-function jc(a){return a?a:!1}var rc=y[5]?y[5][2]:0,sc=rc?rc[4]:!1,tc=rc?rc[5]:!1;!tc||tc instanceof Array||(tc=[tc]);A.fonts=function(a){function c(){window.WebFont.load(a)}25!==sc?c():K(tc,c)};
+function mc(a){return a?a:!1}var uc=y[5]?y[5][2]:0,vc=uc?uc[4]:!1,wc=uc?uc[5]:!1;!wc||wc instanceof Array||(wc=[wc]);A.fonts=function(a){function c(){window.WebFont.load(a)}25!==vc?c():K(wc,c)};
